ea90a7778b0f216b957738e6d7f6d48af82ee732
bulveyz <rus131354@icloud.com>
2018-07-08 12:30:23 +0600

    vue-tabs

---
+++ App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <tabs>
+      <tab name="Home" :selected="true">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos, repellendus.
+      </tab>
+
+      <tab name="About">
+        Lorem ipsum dolor sit amet.
+      </tab>
+
+      <tab name="Support">
+        Lorem ipsum dolor sit amet, consectetur.
+      </tab>
+
+      <tab name="Support">
+        Lorem ipsum dolor sit amet, consectetur.
+      </tab>
+    </tabs>
+  </div>
+</template>
+
+<script>
+  import Tabs from './components/Tabs'
+  import Tab from './components/Tab'
+export default {
+  components: {
+    Tabs,
+    Tab
+  }
+}
+</script>
+
---
+++ Tabs.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <div class="tabs">
+            <ul>
+                <li v-for="tab in tabs" :class="{'is-active': tab.isActive}">
+                    <a href="#" @click="changeTab(tab)">{{tab.name}}</a>
+                </li>
+            </ul>
+        </div>
+        <div class="tab-ditails">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        tabs: []
+      }
+    },
+    created() {
+      this.tabs = this.$children;
+    },
+    methods: {
+      changeTab(changeTab) {
+        this.tabs.forEach(tab => {
+          tab.isActive = (changeTab._uid == tab._uid)
+        });
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
